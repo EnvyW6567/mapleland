00beb33746de585aa309f8a4985794ca9c02916e
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-02-25 18:07:58 +0900

    refactor: parties 타입 shorthand로 변경
    - Array<UserEntity> -> UserEntity[]

--- backend/src/domain/session/type/parties.type.ts
+++ backend/src/domain/session/type/parties.type.ts
@@ -1,14 +1,14 @@
 import { UserEntity } from '../../user/user.entity';
 
 export type Parties = {
-    1: Array<UserEntity>;
-    2: Array<UserEntity>;
-    3: Array<UserEntity>;
-    4: Array<UserEntity>;
-    5: Array<UserEntity>;
-    6: Array<UserEntity>;
-    7: Array<UserEntity>;
-    8: Array<UserEntity>;
-    solo: Array<UserEntity>;
-    observer: Array<UserEntity>;
+    1: UserEntity[];
+    2: UserEntity[];
+    3: UserEntity[];
+    4: UserEntity[];
+    5: UserEntity[];
+    6: UserEntity[];
+    7: UserEntity[];
+    8: UserEntity[];
+    solo: UserEntity[];
+    observer: UserEntity[];
 };
